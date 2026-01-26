05680c0a9f3922b9124c63e45577377f51df1be6
MelaniaRyazanov <melania.ryazanov@gmail.com>
2026-01-26 09:31:12 -0800

    cage class

--- zoo management app/Cage.java
+++ zoo management app/Cage.java
@@ -1,3 +1,35 @@
 public class Cage{
+// Create a private instance variable of type Animal. Don't instantiate the variable, only declare it
    private Animal animal;
+    // No-arg constructor:
+    Cage(){
+       // - Instantiates this cage with a random Animal.
+       Animal animal = new Animal();
+    // Hint: Animal() can choose a random type.
+      
+    }
+    // Constructor with Animal parameter:
+    Cage(Animal animal){
+       // - Places that Animal into the cage.
+      // - Use this.animal to avoid shadowing confusion.
+      this.animal = animal;
+    }
+    
+    // Write a Getter/Accessor for returning the Animal instance variable
+   Animal getAnimal(){
+      return this.animal;
+    } 
+   
+    // Write a Setter/Mutator for editing the Animal instance variable
+   void setAnimal(Animal animal){
+   
+   }
+    public String toString(){
+    // Friendly cage description including the animal.
+      String returnString = "";
+      returnString = "This is the " + animal + " cage.";
+      return returnString;
+  }
+ 
+   
 }
